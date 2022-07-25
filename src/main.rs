93062269785378dcb4337ccc6cd57f6fba1615ab
sir 刘 <sirliu@sirdeMacBook-Pro.local>
2022-07-25 12:36:31 +0800

    first step in vga

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 #![allow(dead_code, unused_imports,non_snake_case,unused_variables,non_upper_case_globals)]
 use core::panic::PanicInfo;
 use core::arch::{asm, global_asm};
+mod vga_buffer;
+
 // mod common;
 // use crate::common::port::Port::InputOutput;
 // use common::{Port};
@@ -15,16 +17,21 @@ static mut vga_x :i8=0;
 static mut vga_y :i8 =0; 
 /*打印字符*/
 static HELLO: &[u8] = b"Hello World!";
+// #[no_mangle]
+// pub extern "C" fn _start() -> ! {
+//     let vga_buffer = 0xb8000 as *mut u8;
+
+//     for (i, &byte) in HELLO.iter().enumerate() {
+//         unsafe {
+//             *vga_buffer.offset(i as isize * 2) = byte;
+//             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+//         }
+//     }
+
+//     loop {}
+// }
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    loop {}
+pub extern "C" fn _start()->!{
+    vga_buffer::print_something();
+    loop{}
 }
